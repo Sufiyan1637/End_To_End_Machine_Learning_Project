b8d7f1ab48cb3b90919f9d0dd19e2c2bca15f9e2
Sufiyan1637 <maleksufiyan8749@gmail.com>
2026-08-14 12:45:19 +0530

    Modified Changes

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -70,4 +70,45 @@ class DataTransformation:
 
 
     def initiate_data_tranformation(self, train_path , test_path):
-        pass
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read Train and Test Data")
+
+            logging.info("Obtaining Preprocesser Object")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column = "math_score"
+            numerical_feature = ["writing_score", "reading_score"]
+
+
+            input_train_feature = train_df.drop(columns=[target_column])
+            target_train_feature = train_df[target_column]
+
+            input_test_feature = test_df.drop(columns=[target_column])
+            target_test_feature = test_df[target_column]
+
+
+            logging.info("Applying Preprocesser Object on Training  and Testing Dataframe")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_train_feature)
+            input_feature_test_arr = preprocessing_obj.fit_transform(input_test_feature)
+
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_train_feature)
+            ]
+
+            test_arr = np.c_[input_feature_test_arr, np.array(target_test_feature)]
+
+            logging.info("Saved Preprocessing Object.")
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocesser_obj_file_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
