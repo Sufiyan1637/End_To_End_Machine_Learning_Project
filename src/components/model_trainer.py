75102cb1ac508e811012faeb224b819b1601c4df
Sufiyan1637 <maleksufiyan8749@gmail.com>
2026-08-20 12:35:23 +0530

    Hyper-Parameter Tunning Added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -40,16 +40,66 @@ class ModelTrainer:
 
 
             models = {
-                "Ada Boost" : AdaBoostRegressor(),
-                "Random Forest" : RandomForestRegressor(),
-                "Gradient Boosting" : GradientBoostingRegressor(),
-                "Decision Tree" : DecisionTreeRegressor(),
-                "K Neighbors" : KNeighborsRegressor(),
-                "Linear Regression" : LinearRegression(),
-                "XG BOOST" : XGBRegressor()
+                        "Random Forest" : RandomForestRegressor(),
+                        "Decision Tree" : DecisionTreeRegressor(),
+                        "Gradient Boosting" : GradientBoostingRegressor(),
+                        "Linear Regression" : LinearRegression(),
+                        "K-Neighbours" : KNeighborsRegressor(),
+                        "XGBRegressor" : XGBRegressor(),
+                        "Ada Boost" : AdaBoostRegressor(),
+                    }
+            
+            
+            params = {
+                "Random Forest" : 
+                {
+                    "criterion" : ["squared_error"  , "absolute_error", "poisson"],
+                    "n_estimators" : [8,16,34,64,128,256],
+                    "max_features" : ["sqrt" , "log2" , None]
+                    
+                },
+                "Decision Tree" :
+                {
+                    "criterion" : ["squared_error"  , "absolute_error", "poisson"],
+                    "splitter" : ["best" , "random"],
+                    "max_features" : ["sqrt" , "log2"]
+                },
+
+
+                "Gradient Boosting" : 
+                {
+                    "loss" : ["squared_error" , "huber" , "absolute_error", "quantile"],
+                    "learning_rate" : [.1 , .01 , .001 , .05],
+                    "subsample" : [.6 , .7 , .75 , .80 , .85 , .90],
+                    "criterion" : ["squared_error" ],
+                    "max_features" : [None , "sqrt" , "log2"],
+                    "n_estimators" : [8,16,32,64,128,256]
+                },
+
+                "Linear Regression" : {},
+
+                "K-Neighbours" :
+                {
+                    "weights" : ['uniform' , 'distance'],
+                    "algorithm" : ['ball_tree' , 'kd_tree' , 'brute']
+                },
+
+                "XGBRegressor" : 
+                {
+                    'learning_rate' : [.1 , .01 , .001 , .05],
+                    'n_estimators' : [8,16,32,64,128,256]
+                },
+
+                "Ada Boost" : 
+                {
+                    'learning_rate' : [.1 , .01 , .001 , .05],
+                    'n_estimators' : [8,16,32,64,128,256]
+                    
+                }
+            
             }
 
-            model_report:dict = evaluate_model(x_train=x_train, x_test=x_test,y_train=y_train, y_test=y_test , models=models)
+            model_report:dict = evaluate_model(x_train=x_train, x_test=x_test,y_train=y_train, y_test=y_test , models=models, params=params)
 
             best_model_score = max(sorted(model_report.values()))
 
