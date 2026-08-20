10b1ba6542e9878098798a75b9ffe23459c36acd
Sufiyan1637 <maleksufiyan8749@gmail.com>
2026-08-20 12:34:37 +0530

    Modified Changes in evaluate_model()

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import os
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path , obj):
@@ -21,18 +22,23 @@ def save_object(file_path , obj):
 
 
 
-def evaluate_model(x_train, y_train, x_test, y_test , models):
+def evaluate_model(x_train, y_train, x_test, y_test , models, params):
 
     report = {}
 
     for i in range(len(list(models))):
         model = list(models.values())[i]
+        param = list(params.values())[i]
+        gc = GridSearchCV(model, param_grid=param, cv=5 , n_jobs=-1)
 
-        model.fit(x_train, y_train)
 
-        y_train_pred = model.predict(x_train)
+        gc.fit(x_train, y_train)
 
-        y_test_pred = model.predict(x_test)
+        best_model = gc.best_estimator_
+
+        y_train_pred = best_model.predict(x_train)
+
+        y_test_pred = best_model.predict(x_test)
 
         train_model_score = r2_score(y_train, y_train_pred)
 
